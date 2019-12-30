af4da46a60d28dbbd8fed46ae17c7a2540f7445a
skjwise <skjwise@hotmail.com>
2019-12-30 14:53:40 +0000

    added local_env file and added JWT tiken code

--- draw_it_backend/app/controllers/users_controller.rb
+++ draw_it_backend/app/controllers/users_controller.rb
@@ -1,42 +1,52 @@
 class UsersController < ApplicationController
 
-    def index 
-        users = User.all
-        render json: users, only: [:id, :username]
-    end
+    # def index 
+    #     users = User.all
+    #     render json: users, only: [:id, :username]
+    # end
 
-    def show
-        user = User.find(params[:id])
-        if user
-            render json: {
-                user: user
-            }
-        else
-            render json: {
-                status: 500, 
-                errors: ['user not found']
-            }
-        end        
-    end
+    # def show
+    #     user = User.find(params[:id])
+    #     if user
+    #         render json: {
+    #             user: user
+    #         }
+    #     else
+    #         render json: {
+    #             status: :unauthorized, 
+    #             errors: ['user not found']
+    #         }
+    #     end        
+    # end
 
-    def create
-        user = User.new(user_params)
-        if user.save
-            login!
-            render json: {
-                status: :created,
-                user: user
-            }
+    # def create
+    #     user = User.new(user_params)
+    #     if user.save
+    #         login!
+    #         render json: {
+    #             status: :created,
+    #             user: user
+    #         }
+    #     else
+    #         render json: {
+    #             status: :unauthorized,
+    #             errors: user.errors.full_messages
+    #         }
+    #     end
+    # end
+
+    def login
+        user = User.find_by(username: user_login_params[:username])
+        if user && user.authenticate(user_login_params[:password])
+            render json: { user: UserSerializer.new(user), token: encode_token(user_id: user.id) }, status: :accepted
         else
-            render json: {
-                status: 500,
-                errors: user.errors.full_messages
-            }
+            render json: { message: 'Invalid email or password' }, status: :unauthorized
         end
     end
 
     private
-    def user_params
+    def user_login_params
         params.require(:user).permit(:username, :password)
     end
+
 end
