89d96efe44803f4fe9a6ae008059002f0bf53069
cseiasp <caroline.seilern@hotmail.com>
2019-12-30 16:52:01 +0000

    fixed serializer bugs, uncommented all methods in user and drawings serializer to make available to front end

--- draw_it_backend/app/serializers/user_serializer.rb
+++ draw_it_backend/app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ActiveModel::Serializer
     attributes :id, :username
     # has_many :drawings
 
-    # def self.all
-    #     User.all.map{|d| UserSerializer.new(d)}
-    # end
+    def self.all
+        User.all.map{|d| UserSerializer.new(d)}
+    end
 end
