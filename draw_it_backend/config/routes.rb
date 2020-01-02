4896751ccff10ac5ed01621e290d6172e958e001
skjwise <skjwise@hotmail.com>
2020-01-02 14:54:56 +0000

    added login and validate user routes

--- draw_it_backend/config/routes.rb
+++ draw_it_backend/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :drawings
   resources :users
+  get '/validate_user', to: 'users#validate_user'
+  post '/login', to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
